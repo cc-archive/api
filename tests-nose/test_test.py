9ac8d7002854101738b3e9b27636967be6f7e9f9
frank <frank@committer.creativecommons.org>
2008-06-20 18:15:38 +0000

    Get trivial tests using the test server, and update TODO.txt

--- tests-nose/test_test.py
+++ tests-nose/test_test.py
@@ -2,16 +2,21 @@
 import os
 import webtest
 
-def test_one():
-    assert True
-
-def test_trivial():
-    assert 2 + 2 == 4
-
-def create_testserver():
+def setup():
+    """Test fixture for nosetests: sets up the WSGI app server
+    """
+    global app
     cfgstr = 'config:%s' % (os.path.join(os.getcwd(), '..', 'server.cfg'))
     app = webtest.TestApp(cfgstr)
-    return app
+
+def test_trivial():
+    assert True
+
+def test_addition():
+    assert 2 + 2 == 4
+
+def test_get():
+    res = app.get('/')
 
 if __name__ == '__main__':
-    create_testserver()
+    pass
