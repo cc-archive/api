3cfe1b2ce8244fea89747cbf75cf4e725a7391e1
frank <frank@committer.creativecommons.org>
2008-06-26 17:24:49 +0000

    split failing tests into their component pieces to further investigate the problem.

--- tests/test_license.py
+++ tests/test_license.py
@@ -26,14 +26,12 @@ class TestLicense(TestApi):
             res = self.app.get('/license/%s' % lclass)
             assert relax_validate(RELAX_LICENSECLASS, res.body)
 
+
+class TestLicenseIssue(TestApi):
+
     def test_issue_invalid_class(self):
         """/issue should return an error with an invalid class."""
         res = self.app.get('/license/blarf/issue?answers=<foo/>')
-        assert relax_validate(RELAX_ERROR, res.body)
-
-    def test_get_invalid_class(self):
-        """/get should return an error with an invalid class."""
-        res = self.app.get('/license/%s/get' % hash(self))
         assert relax_validate(RELAX_ERROR, res.body)
 
     def test_issue_error(self):
@@ -41,7 +39,35 @@ class TestLicense(TestApi):
         res = self.app.get('/license/blarf/issue?answers=<foo/>')
         assert relax_validate(RELAX_ERROR, res.body)
 
-class TestFailure(TestApi):
+    def _issue(self, lclass):
+        """Common /issue testing code."""
+        for answers in self.data.xml_answers(lclass):
+            res = self.app.get('/license/%s/issue?answers=%s' % 
+                                      (lclass, answers))
+            print 'lclass: %s' % lclass
+            print 'answers: %s' % answers
+            print
+            assert relax_validate(RELAX_ISSUE, res.body)
+
+    def test_issue_license_standard(self):
+        """/issue issues standard licenses successfully."""
+        self._issue('standard')
+
+    def test_issue_license_publicdomain(self):
+        """/issue issues publicdomain licenses successfully."""
+        self._issue('publicdomain')
+
+    def test_issue_license_recombo(self):
+        """/issue issues recombo licenses successfully."""
+        self._issue('recombo')
+
+
+class TestLicenseGet(TestApi):
+
+    def test_get_invalid_class(self):
+        """/get should return an error with an invalid class."""
+        res = self.app.get('/license/%s/get' % hash(self))
+        assert relax_validate(RELAX_ERROR, res.body)
 
     def test_get_extra_args(self):
         """Test /get ignores extra nonsense arguments."""
@@ -51,18 +77,24 @@ class TestFailure(TestApi):
                                          (lclass, query_string))
                 assert relax_validate(RELAX_ISSUE, res.body)
 
-    def test_issue(self):
-        """/issue issues every license class successfully."""
-        for lclass in self.data.license_classes():
-            for answers in self.data.xml_answers(lclass):
-                res = self.app.get('/license/%s/issue?answers=%s' %
-                                         (lclass, answers))
-                assert relax_validate(RELAX_ISSUE, res.body)
+    def _get(self, lclass):
+        """Common /get testing code."""
+        for query_string in self.data.query_string_answers(lclass):
+            res = self.app.get('/license/%s/get%s' %
+                                     (lclass, query_string))
+            print 'lclass: %s' % lclass
+            print 'query_string: %s' % query_string
+            print
+            assert relax_validate(RELAX_ISSUE, res.body)
 
-    def test_get(self):
-        """/get issues every license class successfully."""
-        for lclass in self.data.license_classes():
-            for query_string in self.data.query_string_answers(lclass):
-                res = self.app.get('/license/%s/get%s' %
-                                         (lclass, query_string))
-                assert relax_validate(RELAX_ISSUE, res.body)
+    def test_get_license_standard(self):
+        """/get issues standard licenses successfully."""
+        self._get('standard')
+
+    def test_get_license_publicdomain(self):
+        """/get issues publicdomain licenses successfully."""
+        self._get('publicdomain')
+
+    def test_get_license_recombo(self):
+        """/get issues recombo licenses successfully."""
+        self._get('recombo')
